36e4988b8dcf7945bea9428d3b82ae9d7bfbf589
kathmandu777 <mahhakatomanato@gmail.com>
2022-12-12 01:51:40 +0900

    delete: unneed files related to gunicorn

--- fastapi/gunicorn.conf.fastapi.py
+++
@@ -1,6 +0,0 @@
-# WIP: for development
-wsgi_app = "config.asgi:fastapi_app"
-worker_class = "uvicorn.workers.UvicornWorker"
-bind = "0.0.0.0:8000"
-workers = 1
-daemon = True
--- fastapi/scripts/startserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-poetry run gunicorn -c gunicorn.conf.fastapi.py
-poetry run gunicorn -c gunicorn.conf.django.py
