caf0bcdc3366540dfadf19929b9c5b7c77575f2b
kathmandu777 <mahhakatomanato@gmail.com>
2022-12-15 10:36:56 +0900

    fix: enable to access django admin

--- fastapi/config/settings/production.py
+++ fastapi/config/settings/production.py
@@ -8,6 +8,7 @@ ALLOWED_HOSTS = os.environ["ALLOWED_HOSTS"].split(",")
 
 SECURE_SSL_REDIRECT = True
 CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
