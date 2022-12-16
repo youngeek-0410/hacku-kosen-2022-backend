6f40a86c10926f3d8a4a0fdd541c9b409600ea30
m-star18 <31807@toyota.kosen-ac.jp>
2022-12-16 17:18:21 +0900

    fix: refactor spotify router

--- fastapi/app/routers/__init__.py
+++ fastapi/app/routers/__init__.py
@@ -2,8 +2,7 @@ from fastapi import APIRouter
 
 from .health import health_router
 from .message import message_router
-from .project import project_router
-from .spotify import spotify_music_router
+from .project import project_router, spotify_music_router
 
 api_router = APIRouter()
 api_router.include_router(project_router, tags=["project"], prefix="/projects")
