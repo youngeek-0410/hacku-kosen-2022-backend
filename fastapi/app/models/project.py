109beafff00afd326a5d8e0d5fc67697cb9c0e01
kathmandu777 <mahhakatomanato@gmail.com>
2022-12-16 17:46:25 +0900

    fix: migration files

--- fastapi/app/models/project.py
+++ fastapi/app/models/project.py
@@ -1,5 +1,6 @@
-from django.db import models
 import random
+
+from django.db import models
 
 from .base import TimestampModelMixin
 from .message import Message, MessageImage
@@ -14,15 +15,17 @@ class Project(TimestampModelMixin):
     receiver_name = models.CharField(max_length=MAX_LENGTH_RECEIVER_NAME)
 
     MAX_LENGTH_TOP_TEXT = 32
-    top_text = models.CharField(max_length=MAX_LENGTH_TOP_TEXT)
+    top_text = models.CharField(max_length=MAX_LENGTH_TOP_TEXT, default="")
 
     MAX_LENGTH_URL = 256
-    top_image_url = models.URLField(max_length=MAX_LENGTH_URL)
+    top_image_url = models.URLField(max_length=MAX_LENGTH_URL, default="")
 
     MAX_LENGTH_URI = 256
     # TODO: default_spotify_uriの値を追加する(5個くらい?)
     DEFAULT_SPOTIFY_URI = ["spotify:album:6uqewERWZ1vzfCcin1zFIp"]
-    spotify_uri = models.CharField(default=random.choice(DEFAULT_SPOTIFY_URI), max_length=MAX_LENGTH_URI)
+    spotify_uri = models.CharField(
+        default=random.choice(DEFAULT_SPOTIFY_URI), max_length=MAX_LENGTH_URI
+    )
 
     def __str__(self) -> str:
         return f"Project for {self.receiver_name} [{self.id}]"
